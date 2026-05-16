725beff36eba14e3a81e02aa183c95421a7b0090
Nitesh Giri <niteshgiri231@gmail.com>
2026-05-16 16:25:25 +0545

    import userrepo in group module

--- src/group/group.module.ts
+++ src/group/group.module.ts
@@ -4,8 +4,14 @@ import { GroupService } from "./group.service";
 import { GroupMemberController } from "./group-member.controller";
 import { GroupMemberService } from "./group-member.service";
 import { GroupAdminGuard } from "./guard/group-admin.guard";
+import { UserRepository } from "../repository/user.repository";
 @Module({
   controllers: [GroupController, GroupMemberController],
-  providers: [GroupService, GroupMemberService, GroupAdminGuard],
+  providers: [
+    GroupService,
+    UserRepository,
+    GroupMemberService,
+    GroupAdminGuard,
+  ],
 })
 export class GroupModule {}
