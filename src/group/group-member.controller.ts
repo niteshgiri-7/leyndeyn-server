d9ab9e40ac24e907ab2a431cc19ebec9ec61e71f
Nitesh Giri <niteshgiri231@gmail.com>
2026-05-22 00:04:10 +0545

    add join group by invitation code functionality

--- src/group/group-member.controller.ts
+++ src/group/group-member.controller.ts
@@ -12,6 +12,10 @@ import { ALLOW_NON_ADMIN } from "./decorator/allow-non-admin";
 import { CreateGroupMemberDto } from "./dto/create-group-member.dto";
 import { GroupMemberService } from "./group-member.service";
 import { GroupAdminGuard } from "./guard/group-admin.guard";
+import { JoinGroupViaInvitationDto } from "./dto/join-group-via-invitation.dto";
+import { CurrentUser } from "../auth/decorator/current-user.decorator";
+import type { JwtPayload } from "../auth/jwt-payload.type";
+import { GroupRole } from "../../generated/prisma/client/enums";
 
 @UseGuards(AuthGuard, GroupAdminGuard)
 @Controller("group/:groupId/members")
@@ -30,6 +34,17 @@ export class GroupMemberController {
     );
   }
 
+  @Post()
+  async joinGroupByInvitationCode(
+    @CurrentUser() currentUser: JwtPayload,
+    @Body() data: JoinGroupViaInvitationDto,
+  ) {
+    return await this.groupMemberService.joinGroupByInvitationCode(
+      data.invitationCode,
+      currentUser.id,
+    );
+  }
+
   @Delete(":userId")
   async removeMemberFromGroup(
     @Param("groupId") groupId: string,
@@ -45,4 +60,16 @@ export class GroupMemberController {
   }
 
   //TODO: api to change the role of a member in the group
+  @Post("/:memberId/role")
+  async changeMemberRole(
+    @Param("groupId") groupId: string,
+    @Param("memberId") memberId: string,
+    @Body("role") role: GroupRole,
+  ) {
+    return await this.groupMemberService.changeMemberRole(
+      groupId,
+      memberId,
+      role,
+    );
+  }
 }
--- src/group/group-member.service.ts
+++ src/group/group-member.service.ts
@@ -84,6 +84,40 @@ export class GroupMemberService {
     });
   }
 
+  async joinGroupByInvitationCode(invitationCode: string, userId: string) {
+    const group = await this.prisma.group.findUnique({
+      where: {
+        invitationCode,
+      },
+    });
+
+    if (!group) throw new NotFoundException("Invalid invitation code");
+
+    return await this.addMemberToGroup(group.id, userId, "MEMBER");
+  }
+
+  async changeMemberRole(groupId: string, memberId: string, role: GroupRole) {
+    await this.validateGroupExists(groupId);
+
+    const member = await this.prisma.groupMember.findUnique({
+      where: {
+        groupId_userId: {
+          groupId,
+          userId: memberId,
+        },
+      },
+    });
+
+    if (!member) throw new NotFoundException("Member not found in the group");
+
+    return await this.prisma.groupMember.update({
+      where: {
+        groupId_userId: { groupId, userId: memberId },
+      },
+      data: { role },
+    });
+  }
+
   async validateGroupExists(groupId: string) {
     const group = await this.prisma.group.findUnique({
       where: {
