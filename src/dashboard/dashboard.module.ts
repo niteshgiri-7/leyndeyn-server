285d189e5c4bd947b35a426014fb29dde2e3a7f6
Nitesh Giri <niteshgiri231@gmail.com>
2026-07-27 18:13:33 +0545

    add necessary modules in the dashboard imports

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,8 +1,20 @@
 import { Module } from "@nestjs/common";
 import { DashboardController } from "./dashboard.controller";
 import { DashboardService } from "./dashboard.service";
+import { GroupModule } from "../group/group.module";
+import { ExpenseModule } from "../expense/expense.module";
+import { SettlementModule } from "../settlement/settlement.module";
+import { BudgetModule } from "../budget/budget.module";
+import { CategoryModule } from "../category/category.module";
 
 @Module({
+  imports: [
+    GroupModule,
+    ExpenseModule,
+    SettlementModule,
+    BudgetModule,
+    CategoryModule,
+  ],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
