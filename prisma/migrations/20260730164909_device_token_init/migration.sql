9da1123ddeb7260846172f286f24ed41749342a4
Nitesh Giri <niteshgiri231@gmail.com>
2026-07-30 22:37:43 +0545

    deviceToken init

---
+++ prisma/migrations/20260730164909_device_token_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "PlatForm" AS ENUM ('ANDROID', 'IOS');
+
+-- CreateTable
+CREATE TABLE "DeviceToken" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "platform" "PlatForm" NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "DeviceToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DeviceToken_token_key" ON "DeviceToken"("token");
+
+-- AddForeignKey
+ALTER TABLE "DeviceToken" ADD CONSTRAINT "DeviceToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
